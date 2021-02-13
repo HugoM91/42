20689d17ccc1e12d057e8e1aead2dee07244acec
Hugo <hmalaqui@student.42lisboa.com>
2021-02-13 12:39:07 +0000

    update makefile

--- C/libft/ft_memset.c
+++ C/libft/ft_memset.c
@@ -6,43 +6,23 @@
 /*   By: hmalaqui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 10:32:33 by hmalaqui          #+#    #+#             */
-/*   Updated: 2021/02/13 11:34:51 by hmalaqui         ###   ########.fr       */
+/*   Updated: 2021/02/13 12:24:00 by hmalaqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
-{
-	char	*dest;
-	int		s1_len;
-	int		i;
-
-	s1_len = 0;
-	while (s1[s1_len])
-		s1_len++;
-	if (!(dest = (char *)malloc(sizeof(char) * (s1_len + 1))))
-		return (NULL);
-	i = 0;
-	while (i < s1_len)
-	{
-		dest[i] = s1[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-void	*ft_memset(void *b, char c, int len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	char *b_char;
+  
+	b_char = (char *)b;
 
-	*b_char = (char *)b;
 	if (b == NULL)
 		return (NULL);
 	while (*b_char && len > 0)
 	{
-		*b_char = c;
+		*b_char = (unsigned char)c;
 		b_char++;
 		len--;
 	}
