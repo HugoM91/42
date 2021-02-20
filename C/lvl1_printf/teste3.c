3afa5776c9606a530f97d60b5e5aed9793d541cb
Hugo <hmalaqui@student.42lisboa.com>
2021-02-20 09:39:28 +0000

    update

--- C/lvl1_printf/teste3.c
+++ C/lvl1_printf/teste3.c
@@ -6,6 +6,19 @@
 void Myprintf(char *,...); 				//Our printf function
 char* convert(unsigned int, int); 		//Convert integer number into octal, hex, etc.
 
+int	ft_strlen(const char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
 char*
         ft_toupper(char *str)
 {
@@ -62,7 +75,7 @@ long int	ft_abs(long int nbr)
 	return ((nbr < 0) ? -nbr : nbr);
 }
 
-int			ft_len(long int nbr)
+int		ft_len(long long nbr)
 {
 	int		len;
 
@@ -75,7 +88,7 @@ int			ft_len(long int nbr)
 	return (len);
 }
 
-char		*ft_itoa(int n, int base)
+char		*ft_itoa(long long n, int base)
 {
 	int		len;
 	char	*c;
@@ -91,7 +104,7 @@ char		*ft_itoa(int n, int base)
 	if (base == 10){
 		while (len >= 0)
 		{
-			c[len] = '0' + (n % base);
+			c[len] = Representation2[n % base];
 			n = n / base;
 			len--;
 		}
@@ -105,7 +118,31 @@ char		*ft_itoa(int n, int base)
 		}
 	}
 
+			printf("\n\n ===== %s ==== \n\n", c);
 	return (c);
+}
+
+char	*format_x(char *c)
+{
+	int len = ft_strlen(c);
+	char *dest;
+	int i = 0;
+	int h;
+	int j = 0;
+dest=malloc(sizeof(char)*(len+1));
+	while(i < len)
+	{
+		if(c[i] == '0' && i < 2)
+			h = 0;
+		else
+		{
+			dest[j] = c[i];
+			j++;
+		}
+		i++;
+	}
+	dest[j]='\0';
+	return (dest);
 }
 
 
@@ -156,9 +193,8 @@ void Myprintf(char* format,...)
 						} 
                         ft_putstr(ft_itoa(i,10));
 						break; 
-			case 'x': i = va_arg(arg,unsigned int); //Fetch Hexadecimal representation
-						ft_putstr(ft_itoa(i,16));
-						
+			case 'x': i = va_arg(arg,long long); //Fetch Hexadecimal representation
+						ft_putstr(format_x(ft_itoa(i,16)));
 						break; 
             case 'X': i = va_arg(arg,unsigned int); //Fetch Hexadecimal representation
 						ft_putstr(ft_toupper(ft_itoa(i,16)));
@@ -178,13 +214,12 @@ int main()
 { 
 	char *a = "oal tudo mbem";
 	int b = 99999;
-	Myprintf(" ORIGINAL %c", a[0]); 
-    
-    
-	printf("\n0 - %02d\n", 1);
-	printf( "- - %-5d - %-5d\n", 1, 2 );
-	printf( ". - %.5d - %.5d\n", 1, 2 );
-	printf( "* - %*5d - %*57d\n", 1, 2 );
+	Myprintf(" ORIGINAL %c", a[0]);     
+	printf("\n\n %p", &a);
+	printf("\n---------\n");
+	Myprintf("\n\n%x", "aa55555");
+	printf("\n\n%x", "aa55555");
+
 	return 0;
 } 
 /*
@@ -192,12 +227,12 @@ int main()
  * c - Single character (char) value
  * s - String constant or variable (char*)
  * d - Integer (int, short int, long int) - value
- * 
  * i - Integer (int, short int, long int) - value
  * x - Unsigned hexadecimal value, lowercase a–f, no leading 0 or x
  * X - Unsigned hexadecimal value, uppercase A–F, no leading 0 or X
  * 
- * p -  
+ * 
+ * p - Pointer  
  * u - Unsigned integer (unsigned int, unsigned short int, unsigned, long int) value
  * 
  * 
@@ -228,6 +263,6 @@ You would get:
 00010:
 
 
-
+https://docs.microsoft.com/en-us/cpp/c-runtime-library/format-specification-syntax-printf-and-wprintf-functions?view=msvc-160
 
  */
